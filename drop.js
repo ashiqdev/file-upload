698f273ed761f6b076087e41e0cd9eb0ea7621f7
ashiqdev <ashiquejava@gmail.com>
2021-07-12 16:06:08 +0600

    feat:done

--- drop.js
+++ drop.js
@@ -1,80 +1,105 @@
-$(document).ready(function () {
-  initFileUploader('#zdrop');
-  function initFileUploader(target) {
-    var previewNode = document.querySelector('#zdrop-template');
-    previewNode.id = '';
-    var previewTemplate = previewNode.parentNode.innerHTML;
-    previewNode.parentNode.removeChild(previewNode);
+// $(document).ready(function () {
+//   initFileUploader('#zdrop');
+//   function initFileUploader(target) {
+//     var previewNode = document.querySelector('#zdrop-template');
+//     previewNode.id = '';
+//     var previewTemplate = previewNode.parentNode.innerHTML;
+//     previewNode.parentNode.removeChild(previewNode);
 
-    var zdrop = new Dropzone(target, {
-      url: 'http:localost:3000/api',
-      maxFiles: 10,
-      maxFilesize: 30,
-      previewTemplate: previewTemplate,
-      previewsContainer: '#previews',
-      clickable: '#upload-label',
-    });
+//     var zdrop = new Dropzone(target, {
+//       url: 'http:localost:3000/api',
+//       maxFiles: 10,
+//       maxFilesize: 30,
+//       previewTemplate: previewTemplate,
+//       previewsContainer: '#previews',
+//       clickable: '#upload-label',
+//     });
 
-    zdrop.on('addedfile', function (file) {
-      $('.preview-container').css('visibility', 'visible');
-      // $('.filuploader-container').css('height', '30vh');
+//     zdrop.on('addedfile', function (file) {
+//       $('.preview-container').css('visibility', 'visible');
+//       // $('.filuploader-container').css('height', '30vh');
 
-      var windowSize = $(window).width();
-      console.log({ windowSize });
+//       var windowSize = $(window).width();
+//       console.log({ windowSize });
 
-      if (windowSize <= 414) {
-        $('.filuploader-container').css('marginTop', '-140px');
-      } else if (windowSize <= 1250) {
-        $('.filuploader-container').css('marginTop', '-140px');
-      } else {
-        $('.filuploader-container').css('marginTop', '-190px');
-      }
+//       if (windowSize <= 414) {
+//         $('.filuploader-container').css('marginTop', '-140px');
+//       } else if (windowSize <= 1250) {
+//         $('.filuploader-container').css('marginTop', '-140px');
+//       } else {
+//         $('.filuploader-container').css('marginTop', '-190px');
+//       }
 
-      $('.filuploader-container').animate({ height: '300px' }, 500);
-      $('.fileuploader').css('marginTop', '22rem');
-      $('.statistics-wrapper').css('paddingTop', '100px');
-      $('.upload-icon').css('height', '50px');
-      $('.tittle').hide();
-      $('.upload-icon').css('top', '5%');
-    });
+//       $('.filuploader-container').animate({ height: '300px' }, 500);
+//       $('.fileuploader').css('marginTop', '22rem');
+//       $('.statistics-wrapper').css('paddingTop', '100px');
+//       $('.upload-icon').css('height', '50px');
+//       $('.tittle').hide();
+//       $('.upload-icon').css('top', '5%');
+//     });
 
-    // window resize
-    $(window).on('resize', function () {
-      var windowSize = $(window).width();
+//     // window resize
+//     // $(window).on('resize', function () {
+//     //   var windowSize = $(window).width();
 
-      const isCardAvaile = $('.preview-container').is(':visible');
+//     //   const isCardAvaile = $('.preview-container').is(':visible');
 
+//     //   console.log({ isCardAvaile });
 
-      console.log({ isCardAvaile });
+//     //   if (windowSize <= 414) {
+//     //     $('.filuploader-container').css('marginTop', '-140px');
+//     //   } else if (windowSize <= 1250) {
+//     //     console.log('ki');
+//     //     $('.filuploader-container').css('marginTop', '-140px');
+//     //   } else {
+//     //     console.log('boro boro');
+//     //     $('.filuploader-container').css('marginTop', '-190px');
+//     //   }
+//     // });
 
-      if (windowSize <= 414) {
-        $('.filuploader-container').css('marginTop', '-140px');
-      } else if (windowSize <= 1250) {
-        console.log('ki');
-        $('.filuploader-container').css('marginTop', '-140px');
-      } else {
-        console.log('boro boro');
-        $('.filuploader-container').css('marginTop', '-190px');
-      }
-    });
+//     zdrop.on('totaluploadprogress', function (progress) {
+//       var progr = document.querySelector('.progress .determinate');
+//       if (progr === undefined || progr === null) return;
 
-    zdrop.on('totaluploadprogress', function (progress) {
-      var progr = document.querySelector('.progress .determinate');
-      if (progr === undefined || progr === null) return;
+//       progr.style.width = progress + '%';
+//     });
 
-      progr.style.width = progress + '%';
-    });
+//     zdrop.on('dragenter', function () {
+//       $('.fileuploader').addClass('active');
+//     });
 
-    zdrop.on('dragenter', function () {
-      $('.fileuploader').addClass('active');
-    });
+//     zdrop.on('dragleave', function () {
+//       $('.fileuploader').removeClass('active');
+//     });
 
-    zdrop.on('dragleave', function () {
-      $('.fileuploader').removeClass('active');
-    });
+//     zdrop.on('drop', function () {
+//       $('.fileuploader').removeClass('active');
+//     });
+//   }
+// });
 
-    zdrop.on('drop', function () {
-      $('.fileuploader').removeClass('active');
-    });
+const btn = document.querySelector('.upload-icon');
+
+btn.addEventListener('click', () => {
+  console.log('hello');
+  $('.preview-container').css('visibility', 'visible');
+  // $('.filuploader-container').css('height', '2vh');
+
+  var windowSize = $(window).width();
+console.log({ windowSize });
+
+  if (windowSize <= 414) {
+    $('.filuploader-container').css('marginTop', '-140px');
+  } else if (windowSize <= 1250) {
+    $('.filuploader-container').css('marginTop', '0');
+  } else {
+    $('.filuploader-container').css('marginTop', '-140px');
   }
+
+  $('.filuploader-container').animate({ height: '300px' }, 1000);
+  $('.fileuploader').css('marginTop', '22rem');
+  $('.statistics-wrapper').css('paddingTop', '100px');
+  $('.upload-icon').css('height', '50px');
+  $('.tittle').hide();
+  $('.upload-icon').css('top', '5%');
 });
