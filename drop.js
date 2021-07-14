04c90962e933d59c933d40e82527b79e6cb1602b
ashiqdev <ashiquejava@gmail.com>
2021-07-14 21:22:16 +0600

    feat:done

--- drop.js
+++ drop.js
@@ -109,19 +109,19 @@ btn.addEventListener('click', () => {
   $('.tittle').hide();
   $('.upload-icon').css('top', '5%');
 
-  $('.count').each(function () {
-    var $this = $(this);
-    jQuery({ Counter: 0 }).animate(
-      { Counter: $this.attr('data-stop') },
-      {
-        duration: 1600,
-        easing: 'swing',
-        step: function (now) {
-          $this.text(Math.ceil(now));
-        },
-      }
-    );
-  });
+  // $('.count').each(function () {
+  //   var $this = $(this);
+  //   jQuery({ Counter: 0 }).animate(
+  //     { Counter: $this.attr('data-stop') },
+  //     {
+  //       duration: 1600,
+  //       easing: 'swing',
+  //       step: function (now) {
+  //         $this.text(Math.ceil(now));
+  //       },
+  //     }
+  //   );
+  // });
 });
 
 // Pause
